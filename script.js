48f71245c6fba6c55b777af4ba33ce7656b7e971
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2025-11-04 20:06:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,77 +16,78 @@ let btn6 = document.querySelector("#btn6");
 
 btn.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы погладили котика! И он этому рад!")
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы погладили котика! И он этому рад!");
+        tg.MainButton.show();
     }
 }
 
 
 btn1.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар морковь!");
+        item = "морковь";
+        tg.MainButton.show();
     }
 }
 
 btn2.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар кабачок!");
+        item = "кабачок";
+        tg.MainButton.show();
     }
 }
 
 btn3.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар картофель!");
+        item = "картофель";
+        tg.MainButton.show();
     }
 }
 
 btn4.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар баклажан!");
+        item = "баклажан";
+        tg.MainButton.show();
     }
 }
 
 btn5.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар помидор!");
+        item = "помидор";
+        tg.MainButton.show();
     }
 }
 
 btn6.onclick = () => {
     if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        // item = "морковь"
-        tg.MainButton.show()
+        tg.MainButton.setText("Вы выбрали товар огурец!");
+        item = "огурец";
+        tg.MainButton.show();
     }
 }
 
 // Telegram.WebApp.onEvent("mainButtonClicked", function() {
 //     tg.sendData(item);
 // })
+
 
 
 
