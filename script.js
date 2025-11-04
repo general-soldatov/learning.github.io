877718bbee287a45cef3d83f14a93725535872e9
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2025-11-04 19:37:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "2cab37";
+// tg.MainButton.textColor = "#FFFFFF";
+// tg.MainButton.color = "2cab37";
 
-let item = "";
+// let item = "";
 
-let btn = document.querySelector("#click");
-let btn1 = document.querySelector("#btn1");
-let btn2 = document.querySelector("#btn2");
-let btn3 = document.querySelector("#btn3");
-let btn4 = document.querySelector("#btn4");
-let btn5 = document.querySelector("#btn5");
-let btn6 = document.querySelector("#btn6");
+// let btn = document.querySelector("#click");
+// let btn1 = document.querySelector("#btn1");
+// let btn2 = document.querySelector("#btn2");
+// let btn3 = document.querySelector("#btn3");
+// let btn4 = document.querySelector("#btn4");
+// let btn5 = document.querySelector("#btn5");
+// let btn6 = document.querySelector("#btn6");
 
 btn.onclick = () => {
     if (tg.MainButton.isVisible) {
@@ -24,67 +24,68 @@ btn.onclick = () => {
 }
 
 
-btn1.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn1.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
-btn2.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn2.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
-btn3.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn3.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
-btn4.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn4.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
-btn5.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn5.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
-btn6.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!")
-        item = "морковь"
-        tg.MainButton.show()
-    }
-}
+// btn6.onclick = () => {
+//     if (tg.MainButton.isVisible) {
+//         tg.MainButton.hide()
+//     } else {
+//         tg.MainButton.setText("Вы выбрали товар морковь!")
+//         item = "морковь"
+//         tg.MainButton.show()
+//     }
+// }
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
     tg.sendData(item);
 })
+
 
