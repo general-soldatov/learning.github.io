7973d9be225a32eb20fd613a6b048398ba9f70ed
Юрий Солдатов <141447194+general-soldatov@users.noreply.github.com>
2025-11-04 22:12:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+ tg.MainButton.textColor = "#FFFFFF";
+ tg.MainButton.color = "#2cab37";
 
-let item = "";
+let name = "";
+let phone = "";
+let email = "";
+let items = {
+    carrot: 0,
+    zukkiny: 0,
+    potato: 0,
+    eggplant: 0,
+    tomato: 0,
+    cucumber: 0
+}
+
+let price = 0;
+
+let prices = {
+    carrot: 5,
+    zukkiny: 3,
+    potato: 2,
+    eggplant: 8,
+    tomato: 4,
+    cucumber: 2
+}
 
 let btn = document.querySelector("#click");
 let btn1 = document.querySelector("#btn1");
@@ -13,6 +34,29 @@ let btn3 = document.querySelector("#btn3");
 let btn4 = document.querySelector("#btn4");
 let btn5 = document.querySelector("#btn5");
 let btn6 = document.querySelector("#btn6");
+
+let username = document.querySelector("#user-name");
+let userphone = document.querySelector("#user-phone");
+let usermail = document.querySelector("#user-email");
+
+let submit = document.querySelector("#submit");
+
+submit.onclick = () => {
+    tg.MainButton.setText("Нажмите кнопку для оформления заказа!");
+    tg.MainButton.show();
+}
+
+username.onchange = () => {
+    name = username.value;
+}
+
+userphone.onchange = () => {
+    phone = userphone.value;
+}
+
+usermail.onchange = () => {
+    email = usermail.value;
+}
 
 btn.onclick = () => {
     if (tg.MainButton.isVisible) {
@@ -25,73 +69,90 @@ btn.onclick = () => {
 
 
 btn1.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар морковь!");
-        item = "морковь";
-        tg.MainButton.show();
-    }
+//    tg.MainButton.setText("Вы выбрали товар морковь!");
+    items['carrot'] += 1;
+//    tg.MainButton.show();
+    update_orders();
 }
 
 btn2.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар кабачок!");
-        item = "кабачок";
-        tg.MainButton.show();
-    }
+    items['zukkiny'] += 1;
+    update_orders();
 }
 
 btn3.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар картофель!");
-        item = "картофель";
-        tg.MainButton.show();
-    }
+    items['potato'] += 1;
+    update_orders();
 }
 
 btn4.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар баклажан!");
-        item = "баклажан";
-        tg.MainButton.show();
-    }
+    items['eggplant'] += 1;
+    update_orders();
 }
 
 btn5.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар помидор!");
-        item = "помидор";
-        tg.MainButton.show();
-    }
+    items['tomato'] += 1;
+    update_orders();
 }
 
 btn6.onclick = () => {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар огурец!");
-        item = "огурец";
-        tg.MainButton.show();
+    items['cucumber'] += 1;
+    update_orders();
+}
+
+function update_orders() {
+    while (usercard.hasChildNodes()) {
+        usercard.removeChild(usercard.lastChild);
+    }
+    usercard.innerHTML = "Ваши заказы";
+    if (name != "") {
+        let userinfo = document.createElement("li");
+        userinfo.innerHTML = "Имя - " + name;
+        usercard.appendChild(userinfo);
+    }
+    if (phone != "") {
+        let userinfo = document.createElement("li");
+        userinfo.innerHTML = "Телефон - " + phone;
+        usercard.appendChild(userinfo);
+    }
+    if (email != "") {
+        let userinfo = document.createElement("li");
+        userinfo.innerHTML = "Email - " + email;
+        usercard.appendChild(userinfo);
+    }
+    price = 0;
+    for (let item in items) {
+        if (items[item] != 0) {
+            let li = document.createElement("li");
+            li.innerHTML = item + ": " + items[item];
+            usercard.appendChild(li);
+            price += items[item] * prices[item];
+        }
+    }
+    if (price != 0) {
+        let userinfo = document.createElement("li");
+        userinfo.innerHTML = "Итого: " + price + "$";
+        usercard.appendChild(userinfo);
     }
 }
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(item);
+    let result = "";
+    if (name != "") {
+        result += "Ваше имя: " + name + "\n";
+    }
+    if (phone != "") {
+        result += "Ваш телефон: " + phone + "\n";
+    }
+    if (email != "") {
+        result += "Ваш email: " + email + "\n";
+    }
+    result += "Ваш заказ: " + "\n";
+    for (let item in items) {
+        if (items[item] != 0) {
+            result +=  item + ": " + items[item] + "\n";
+        }
+    }
+    result += "\n\nС вас " + price + "$";
+    tg.sendData(result);
 })
-
-
-
-
-
-
-
-
